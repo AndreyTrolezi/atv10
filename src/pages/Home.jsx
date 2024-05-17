e2c728609952b36b5a552d9c2c5e8f76fb31a52f
Andrey Trolezi <laureano.yane@gmail.com>
2024-05-17 18:36:34 +0000

    criação dos primeiros estilos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HomeContainer } from './Home.styles'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <h1>
+        a
+      </h1>
+    </HomeContainer>
+  )
+}
